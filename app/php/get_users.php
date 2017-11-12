5f7fe93e422e6746b9534dd98e8a8751965122ed
yorvie <droumatt@yahoo.fr>
2017-11-12 18:25:57 +0100

    user table on modo page

--- app/php/get_users.php
+++ app/php/get_users.php
@@ -4,7 +4,7 @@
 
     include("dao.php");
 
-    $selected_fields = array("id", "username", "login", "server_id", "email", "ip");
+    $selected_fields = array("id", "username", "login", "server_id", "email", "ip", "right_id");
 
     // Récupération des utilisateurs
 
